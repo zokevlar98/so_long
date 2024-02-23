eae72dcc86584035b00658db08ea279d301afd7b
Ziad Qouri <zqouri@e2r8p12.1337.ma>
2024-02-23 23:30:50 +0100

    first commit

---
+++ includes/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 23:25:41 by zqouri            #+#    #+#             */
+/*   Updated: 2024/02/23 23:26:38 by zqouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stddef.h>
+
+#endif
